096ad06f06067431dcab9e08c8b9a61a674e8e0e
Saheed Akinbile <sakin070@uottawa.ca>
2019-10-12 20:42:39 -0400

    Fixed the file path

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout/Layout"
 import Hero from "../components/hero/Hero"
 import Summary from "../components/summary/Summary"
 import Projects from "../components/projects/Projects"
-import About from "/Users/saheed/GitHub/saheed-portfolio/src/pages/about.js"
+import About from "./about.js"
 
 //import bootstrap styles
 import "../bootstrap/css/bootstrap.min.css"
